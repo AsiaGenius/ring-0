0b043dab51916bd8464a45e818b640ec64a26879
Magdiel <magdiel.linhares@live.com>
2016-11-25 21:35:16 -0300

    Crypto == Temporarily disabled, but does not interfere with shield functionality.

--- info/infos/dllmain.cpp
+++ info/infos/dllmain.cpp
@@ -20,7 +20,7 @@ void loadConf()
 //////////////////////////////////////////////////////////////////////////
 
 carrega.UserIP = "127.0.0.1";	//you server IP
-carrega.UserHex =  "myro.exe";	//hexed name
+carrega.UserHex =  "my_ro.exe";	//hexed name
 carrega.MaxAllowedInstances =  2;	//Number of max game client, MAX: 99 (Recomended: 2)
 
 //////////////////////////////////////////////////////////////////////////
@@ -30,18 +30,19 @@ carrega.MaxAllowedInstances =  2;	//Number of max game client, MAX: 99 (Recomend
 //List here all protected files, eg.: #define ProtectA "my_CRC:filename"
 //Max number of protect file is 10
 //All file type allowed
-carrega.NumberOfPrt = 1;		//Number  of protected files Default: 3 (only 3 file protected in this case)
 
-carrega.ProtectA = "0x5339965e:data.grf";
-carrega.ProtectB =  "0x5339965e:my_ro.exe";
-carrega.ProtectC = "0x5339965e:my_ro.grf";
-carrega.ProtectD = "0x5339965e:DATA.INI";
-carrega.ProtectE = "0x5339965e:files.grf";
-carrega.ProtectF = "0x5339965e:palleta.grf";
-carrega.ProtectG = "0x5339965e:jobs.grf";
-carrega.ProtectH = "0x5339965e:cps.dll";
+carrega.NumberOfPrt = 1;		//Number  of protected files Default: 3 (only 3 file protected in this case)	//DISABLED TEMPORARY
+
+carrega.ProtectA = "0x5339965e:data.grf";	//DISABLED TEMPORARY
+carrega.ProtectB =  "0x5339965e:my_ro.exe";	//DISABLED TEMPORARY
+carrega.ProtectC = "0x5339965e:my_ro.grf";	//DISABLED TEMPORARY
+carrega.ProtectD = "0x5339965e:DATA.INI";	//DISABLED TEMPORARY
+carrega.ProtectE = "0x5339965e:files.grf";	//DISABLED TEMPORARY
+carrega.ProtectF = "0x5339965e:palleta.grf";	//DISABLED TEMPORARY
+carrega.ProtectG = "0x5339965e:jobs.grf";	//DISABLED TEMPORARY
+carrega.ProtectH = "0x5339965e:cps.dll";	//DISABLED TEMPORARY
 carrega.ProtectI = "0x5339965e:GameGuard/ring.dll";		//    <<<---  IMPORTANT!!!!! IMPORTANT!!!!!!!!! ATTENTION!!!  GameGuard/ring.dll
-carrega.ProtectJ = "0x5339965e:BGM/1.mp3";
+carrega.ProtectJ = "0x5339965e:BGM/1.mp3";	//DISABLED TEMPORARY
 
 //Attention! Max 10 protected file allowed, if you dont need protect 10 files, set NumberOfPrt to you number of protected file
 
