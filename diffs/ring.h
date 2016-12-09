756d75a822f12e8d39644095f885ef732cfa9d4c
Magdiel <magdiel.linhares@live.com>
2016-12-09 17:51:14 -0300

    Release 2.13 - Rathena - Enjoy!

--- diffs/ring.h
+++ diffs/ring.h
@@ -1,23 +1,7 @@
 //Ring-0	your crc_ring need lengt 32 characters - If it exceeds 32 characters delete the last characters until 32.
 //d7d7da735c6d310f884558f2313aa457		
-#define CRC_RING "d7d7da735c6d310f884558f2313aa457" //paste your CPT32 here <<<<<<
-struct {
-  char key[10];
-} personA, person_copyA;
-
-struct {
-  char macc[18];
-} personB, person_copyB;
-
-struct {
-  char hdid[33];
-} personC, person_copyC;
-
-
-struct {
-  char keyzim[34];
-} personD, person_copyD;
-
-
+//d7da735c6d310f884558f2313aa457
+//d7d7da735c6d310f884558f2313aa457
+#define CRC_RING "d7d7da735c6d310f884558f2313aa4" //paste your CPT32 here <<<<<<
 int statusS;
 
