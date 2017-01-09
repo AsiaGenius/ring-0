a9dcd236b1ffe31bc35796f176113cccc4cdfea0
Magdiel <magdiel.linhares@live.com>
2017-01-09 13:15:27 -0300

    eAmod and rAmod Diffs

---
+++ diffs/eAmod/src/login/ring.h
@@ -0,0 +1,7 @@
+//Ring-0	your crc_ring need lengt 32 characters - If it exceeds 32 characters delete the last characters until 32.
+//d7d7da735c6d310f884558f2313aa457		
+//d7da735c6d310f884558f2313aa457
+//d7d7da735c6d310f884558f2313aa457
+#define CRC_RING "15423c602f4b8bc0b2e769c841c0a1" //paste your CPT32 here <<<<<<
+int statusS;
+
