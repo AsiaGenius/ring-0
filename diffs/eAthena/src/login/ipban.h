417611f585664d1c4e8efe597c947b93617f3d8b
Magdiel <magdiel.linhares@live.com>
2017-02-08 20:26:01 -0300

    eAthena Setup Diff

---
+++ diffs/eAthena/src/login/ipban.h
@@ -0,0 +1,30 @@
+// Copyright (c) Athena Dev Teams - Licensed under GNU GPL
+// For more information, see LICENCE in the main folder
+
+#ifndef __IPBAN_H_INCLUDED__
+#define __IPBAN_H_INCLUDED__
+
+#include "../common/cbasetypes.h"
+
+// initialize
+void ipban_init(void);
+
+// finalize
+void ipban_final(void);
+
+// check ip against ban list
+bool ipban_check(uint32 ip);
+
+// increases failure count for the specified IP
+void ipban_log(uint32 ip);
+
+// parses configuration option
+bool ipban_config_read(const char* key, const char* value);
+
+//Ring-0 
+void update_last_data(const char* account_id, const char* hwid, const char* colun);
+int ring_ban_check(char *hwid);
+char* CpyData(char *inputS, char* delS);
+//
+
+#endif // __IPBAN_H_INCLUDED__
