0663f0c3fb1ace2c11a2f8c78b60ea5a6d77edc3
Magdiel <magdiel.linhares@live.com>
2017-06-15 09:27:52 -0300

    Other option for eAmod emus

---
+++ diffs/eAmod Option #2/ring.h
@@ -0,0 +1,6 @@
+//Ring-0	your crc_ring need lengt 32 characters - If it exceeds 32 characters delete the last characters until 32.
+//d7d7da735c6d310f884558f2313aa457		
+//d7da735c6d310f884558f2313aa457
+//d7d7da735c6d310f884558f2313aa457
+#define CRC_RING "d7d7da735c6d310f884558f2313aa4" //paste your CPT32 here <<<<<<
+int statusS;
